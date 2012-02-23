ecfbf868e6eaa68189490dd707db595a14513e68
Gahoo Lee <gahoolee@gmail.com>
2012-02-23 22:18:59 +0800

    dump Motif in specific SeqNames

---
+++ dumpCSV.py
@@ -0,0 +1,93 @@
+#! /usr/bin/env python
+#coding=utf-8
+import os
+import sys
+import time
+import getopt
+from zipfile import ZipFile, ZIP_DEFLATED
+from CAREdb import CAREdb
+
+def dumpCSV(dbname,SeqName_list):
+    print "dumpCSV"
+    CSV={}
+    care=CAREdb(dbname)
+    SeqName=invertDict(care.SeqName)
+    Motif=invertDict(care.Motif)
+    Organism=invertDict(care.Organism)
+
+    REF_SeqName_list=[str(care.SeqName[sn]) for sn in SeqName_list]
+    for rs in readSeqName(care,REF_SeqName_list):
+        (REF_SeqName,REF_Motif,REF_Organism,Description,start,stop,strand,pValue)=rs
+        if strand==1:
+            strand='+'
+        else:
+            strand='-'
+        Description=Description.replace(',',';')
+        row=(Motif[REF_Motif],Organism[REF_Organism],Description,start,stop,strand,pValue)
+        row=[str(c) for c in row]
+        if CSV.has_key(SeqName[REF_SeqName]):
+            CSV[SeqName[REF_SeqName]].append(",".join(row))
+        else:
+            CSV[SeqName[REF_SeqName]]=[",".join(row)]
+    return CSV
+
+def invertDict(d):
+    return dict([(v,k) for k,v in d.items()])
+
+def readSeqName(care,REF_SeqName_list):
+    print "Load2RAM"
+    care.cache('Instance')
+    care.cache('Scanned',['REF_SeqName','REF_MotifSeq','start','stop','strand','pValue'],SeqName=REF_SeqName_list)
+    SQL="""
+    SELECT REF_SeqName,REF_Motif,REF_Organism,Description,start,stop,strand,pValue
+    FROM cache.Instance,cache.Scanned
+    WHERE cache.Instance.REF_MotifSeq=cache.Scanned.REF_MotifSeq
+    AND REF_SeqName in ('%s')
+    """ % "','".join(REF_SeqName_list)
+    #print SQL
+    care.cur.execute(SQL)
+    return care.cur.fetchall()
+
+def writeCSV(outfile,CSV):
+    print "writting CSV"
+    zf=ZipFile(outfile, "w", ZIP_DEFLATED)
+    for SeqName in CSV.keys():
+        content="\n".join(CSV[SeqName])
+        zf.writestr(SeqName+".csv",content)
+    zf.close()
+
+def usage():
+    print "dumpCSV.py -d <dbfile> -i <SeqName_list> [-o] <outfile> "
+
+def loadList(filename):
+    return [line.strip() for line in open(filename).readlines()]
+
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hd:i:o:", ["help","database=", "filename=", "outfile="])
+        if len(opts)<2:
+            usage()
+            sys.exit(2)
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif opt in ("-d", "--database"):
+            dbname=arg
+            if not os.path.exists(dbname):
+                raise Exception,"There is no %s here" % dbname
+        elif opt in ("-i", "--filename"):
+            filename = arg
+            if not os.path.exists(filename):
+                raise Exception,"There is no %s here" % filename
+            outfile = "%s.zip" % filename.split('.')[0]
+        elif opt in ("-o", "--outfile"):
+            outfile = arg
+    timestamp=time.time()
+    SeqName_list=loadList(filename)
+    CSV=dumpCSV(dbname,SeqName_list)
+    writeCSV(outfile,CSV)
+    print time.time()-timestamp
