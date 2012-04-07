6a7a0578b8b21c3dd6a62ada47d6f1578f175a3b
Gahoo Lee <gahoolee@gmail.com>
2012-04-07 22:39:12 +0800

    added Node description

--- Stats.py
+++ Stats.py
@@ -5,6 +5,8 @@ import os
 import time
 import getopt
 import cPickle
+from scipy import stats
+from sqlite3 import OperationalError
 from math import log, exp, sqrt
 from mpmath import loggamma
 from zipfile import ZipFile, ZIP_DEFLATED
@@ -39,6 +41,9 @@ def gauss_hypergeom(X, n, m, N):
     return exp(r1 + r2 - r3)
 
 def hypergeo_cdf(X, n, m, N):
+    '''
+    用来计算共现的p
+    '''
 
     assert N >= m, 'Number of items %i must be larger than the number of marked items %i' % (N, m)
     assert m >= X, 'Number of marked items %i must be larger than the number of sucesses %i' % (m, X)
@@ -48,6 +53,25 @@ def hypergeo_cdf(X, n, m, N):
 
     s = 0
     for i in range(X, min(m,n)+1):
+        s += max(gauss_hypergeom(i, n, m, N), 0.0)
+    return min(max(s,0.0), 1)
+
+def hypergeo_cdf_enrich(X, n, m, N):
+    '''
+    用来计算富集的p
+    N：数据库中基因总数
+    m：数据库中特定motif数
+    n：列表中基因总数
+    x：列表中特定motif数
+    '''
+    assert N >= m, 'Number of items %i must be larger than the number of marked items %i' % (N, m)
+    assert m >= X, 'Number of marked items %i must be larger than the number of sucesses %i' % (m, X)
+    assert n >= X, 'Number of draws %i must be larger than the number of sucesses %i' % (n, X)
+    assert N >= n, 'Number of draws %i must be smaller than the total number of items %i' % (n, N)
+    assert N-m >= n-X, 'There are more failures %i than unmarked items %i' % (N-m, n-X)
+
+    s = 0
+    for i in range(0, X+1):
         s += max(gauss_hypergeom(i, n, m, N), 0.0)
     return min(max(s,0.0), 1)
 
@@ -62,10 +86,12 @@ def Stats(dbname, SeqName=None, Motifs=None, p=None, output=None):
         basename=care.dbfile.split('.')[0]
     else:
         basename=output
+    checkMotif_Counts(care)
     (REF_SeqName,Motifs)=loadDB2RAM(care,SeqName,Motifs)
     id2MotifSeq=dict([(v,k) for k,v in care.MotifSeq.items()])
     motif_dist={}
     Motif_SeqName={}
+    Motif_Enrich={}
     for motif in Motifs:
         REF_MotifSeq=getREF_MotifSeq(care,motif)
         motif_dist[motif]={}
@@ -74,22 +100,28 @@ def Stats(dbname, SeqName=None, Motifs=None, p=None, output=None):
             MotifSeq=id2MotifSeq[MotifSeq_id]
             motif_dist[motif][MotifSeq]=getDist(care,MotifSeq_id)
             Motif_SeqName[motif][MotifSeq]=getREF_SeqName(care,MotifSeq_id)
-    MergedSeqName=getMergedSeqName(Motif_SeqName)
+    MergedSeqName=getMerged(Motif_SeqName,'SeqName')
+    Merged_dist=getMerged(motif_dist,'dist')
     SeqName_counts=countSeqName(MergedSeqName)
     zf = ZipFile("%s.zpkl" % basename, 'w', ZIP_DEFLATED)
+    if SeqName:
+        Enriched=enrichMent(care,SeqName_counts,list_size=len(REF_SeqName),output=output)
+        zf.writestr('Enriched.pkl', cPickle.dumps(Enriched))
     zf.writestr('motif_dist.pkl', cPickle.dumps(motif_dist))
+    zf.writestr('Merged_dist.pkl', cPickle.dumps(Merged_dist))
     zf.writestr('Motif_SeqName.pkl', cPickle.dumps(Motif_SeqName))
     zf.writestr('MergedSeqName.pkl', cPickle.dumps(MergedSeqName))
     zf.writestr('SeqName_counts.pkl', cPickle.dumps(SeqName_counts))
+    co_Occur=coOccur(care,REF_SeqName,SeqName_counts,MergedSeqName,p,output)
+    zf.writestr('co_Occur.pkl', cPickle.dumps(co_Occur))
     zf.close()
-    coOccur(care,REF_SeqName,SeqName_counts,MergedSeqName,p,output)
 
 def loadDB2RAM(care,SeqName,Motifs):
     '''
     将指定部分数据库载入内存备用
     '''
     if SeqName:
-        REF_SeqName=[care.SeqName[sn] for sn in SeqName]
+        REF_SeqName=[care.SeqName[sn] for sn in SeqName if care.SeqName.has_key(sn)]
     else:
         REF_SeqName=None
     if Motifs:
@@ -169,6 +201,52 @@ def getREF_SeqName(care,MotifSeq_id,strand='both'):
             SeqName['-'].append(REF_SeqName)
     return SeqName
 
+def enrichMent(care,SeqName_counts,list_size,output=None):
+    '''
+    计算Motif是否在输入列表中富集
+    '''
+    if not output:
+        basename=care.dbfile.split('.')[0]
+    else:
+        basename=output
+    db_size=len(care.SeqName)
+    nodefile="%s.Node" % basename
+    enrichNodefile="%s_enriched.Node" % basename
+    Motif_Desc=care.loadDB2dict('Motif',1,2)
+    node=[]
+    enrichNode=[]
+    Enriched={}
+    for motif in SeqName_counts.keys():
+        print motif
+        REF_Motif=care.Motif[motif]
+        x=SeqName_counts[motif]
+        m=care.Motif_Counts[REF_Motif]
+        if not x:
+            print "not found"
+            continue
+        ER=(float(x)/list_size)/(float(m)/db_size)
+        print x,m,list_size,db_size,ER,stats.hypergeom.cdf(x,db_size,m,list_size)
+        try:
+            p=1-hypergeo_cdf_enrich(x,list_size,m,db_size)
+        except AssertionError,ERR:
+            #17,285,41829,42086
+            print ERR
+            p=1
+        print x,m,list_size,db_size,ER,p
+        row=(motif,Motif_Desc[motif],x,m,list_size,db_size,ER,p)
+        row=[str(r) for r in row]
+        node.append("\t".join(row) )
+        if p<0.05:
+            Enriched[motif]={}
+            Enriched[motif]['ER']=ER
+            Enriched[motif]['p']=p
+            enrichNode.append("\t".join(row) )
+    NodeOutput=open(nodefile, 'w')
+    NodeOutput.write("\n".join(node))
+    enrichNodefile=open(enrichNodefile, 'w')
+    enrichNodefile.write("\n".join(enrichNode))
+    return Enriched
+
 def coOccur(care,REF_SeqName,SeqName_counts,MergedSeqName,p=None,output=None):
     '''
     共现的Motif
@@ -179,11 +257,8 @@ def coOccur(care,REF_SeqName,SeqName_counts,MergedSeqName,p=None,output=None):
         basename=care.dbfile.split('.')[0]
     else:
         basename=output
-    nodefile="%s.Node" % basename
     edgefile="%s.Edge" % basename
-    NodeOutput=open(nodefile, 'w')
     EdgeOutput=open(edgefile, 'w')
-    node=[]
     edge=[]
     co_Occur={}
     print "-------"
@@ -197,7 +272,6 @@ def coOccur(care,REF_SeqName,SeqName_counts,MergedSeqName,p=None,output=None):
         #p=1.0/(m_total_num*(m_total_num-1))
     for motifa in MergedSeqName.keys():
         motifA=MergedSeqName[motifa]
-        node.append("%s\t%s" % (motifa,SeqName_counts[motifa]))
         print len(MergedSeqName)
         MergedSeqName.pop(motifa)
         for motifb in MergedSeqName.keys():
@@ -209,34 +283,47 @@ def coOccur(care,REF_SeqName,SeqName_counts,MergedSeqName,p=None,output=None):
             b_num=SeqName_counts[motifb]
             hypergeocdf=hypergeo_cdf(co_num,a_num,b_num,Seq_num)
             if hypergeocdf<=p:
-                row=[motifa,motifb,co_num,hypergeocdf]
+                row=[motifa,motifb,co_num,a_num,b_num,hypergeocdf]
                 row="\t".join([str(col) for col in row])
                 #print row
                 edge.append(row)
-    NodeOutput.write("\n".join(node))
     EdgeOutput.write("\n".join(edge))
+    return co_Occur
 
-def getMergedSeqName(Motif_SeqName):
+def getMerged(motif_dict,type):
     '''
     合并Motif_SeqName结果，即把：
     Motif_SeqName[motif][MotifSeq]={'+':SeqName,'-':SeqName}
     变成：
     Motif_SeqName[motif]=SeqName
     '''
-    MergedSeqName={}
-    for motif in Motif_SeqName.keys():
-        MergedSeqName[motif]=mergeSeqName(Motif_SeqName[motif])
-    return MergedSeqName
+    Merged={}
+    if type=='dist':
+        for motif in motif_dict.keys():
+            Merged[motif]=merge(motif_dict[motif])
+    elif type=='SeqName':
+        for motif in motif_dict.keys():
+            Merged[motif]=mergeSeqName(motif_dict[motif])
+    return Merged
 
-def mergeSeqName(motif_dict):
+def merge(motif_dict):
     '''
     motif_dict={MotifSeq1:{'+':[],'-':[]},}
     '''
     Seq_id=[]
     [Seq_id.extend(v['+']+v['-']) for v in motif_dict.values()]
-    return set(Seq_id)
+    return Seq_id
+
+def mergeSeqName(motif_dict):
+    '''
+    motif_dict={MotifSeq1:{'+':[],'-':[]},}
+    '''
+    return set(merge(motif_dict))
 
 def countSeqName(MergedSeqName):
+    '''
+    统计SeqName数目
+    '''
     SeqName_counts={}
     for motif in MergedSeqName.keys():
         SeqName_counts[motif]=len(MergedSeqName[motif])
@@ -250,6 +337,9 @@ def distance():
     return
 
 def loadList(filename):
+    '''
+    加载列表文件内容
+    '''
     lst=list(set([line.strip() for line in open(filename).readlines()]))
     print "%s loaded" % len(lst)
     try:
@@ -258,14 +348,45 @@ def loadList(filename):
         pass
     return lst
 
+def checkMotif_Counts(care):
+    '''
+    检查数据库内Motif_Counts是否有内容，没有就统计
+    '''
+    if not care.Motif_Counts:
+        print "first stats"
+        countAllSeqNameInDB(care)
+        care.Motif_Counts=care.loadDB2dict('Motif_Counts',0,1)
+
+def countAllSeqNameInDB(care):
+    '''
+    统计数据库内全部Motif对应的SeqName数目
+    #TODO 必须注意，如果选取的序列长度不同应该重新进行计算，否则背景是不同的，会导致Enrichment的计算有误，可以考虑增加一列记录长度的字段
+    '''
+    SQL="""
+    INSERT INTO Motif_Counts (REF_Motif,SeqName_counts)
+    VALUES (%s,(
+        SELECT COUNT(DISTINCT REF_SeqName)
+        FROM Scanned
+        WHERE REF_MotifSeq in (
+            SELECT DISTINCT REF_MotifSeq
+            FROM Instance
+            WHERE REF_Motif=%s)
+                ))
+    """
+    for REF_Motif in care.Motif.values():
+        print REF_Motif
+        care.cur.execute(SQL % (REF_Motif,REF_Motif))
+    care.commit()
+
 def usage():
-    print "CAREdb.py -f <fasta_file> [-d] <dbfile> [-s]<seqname_list> [-m] <motif_list> [-p] pThresh [-o] <output>"
+    print "Stats.py -f <fasta_file> [-d] <dbfile> [-s]<seqname_list> [-m] <motif_list> [-p] pThresh [-o] <output>"
 
 def has_file(filename):
     if not os.path.exists(filename):
         raise Exception,"There is no %s here" % filename
 
 if __name__ == '__main__':
+    (seqname_list,motif_list,pThresh)=(None,None,None)
     try:
         opts, args = getopt.getopt(sys.argv[1:], "hd:s:m:p:o:", \
             ["help","database=","seqname_list=","motif_list=","pThresh","output="])
@@ -279,9 +400,6 @@ if __name__ == '__main__':
         elif opt in ("-d", "--database"):
             dbname=arg
             has_file(dbname)
-            seqname_list=None
-            motif_list=None
-            pThresh=None
             output=dbname.split('.')[0]
         elif opt in ("-s", "--seqname_list"):
             seqname_list=arg
@@ -301,5 +419,6 @@ if __name__ == '__main__':
         usage()
         sys.exit(2)
     timestamp=time.time()
+    #第一次执行的时候统计数据库中各类Motif对应的SeqName数量，以便看是否在给定列表中enrich
     Stats(dbname, seqname_list, motif_list, pThresh, output)
     print time.time()-timestamp
