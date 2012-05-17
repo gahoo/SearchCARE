2e402bac9599f010222c58fb33d7c3e6e3453c16
Gahoo Lee <gahoolee@gmail.com>
2012-05-17 10:53:51 +0800

    reduce MEM usage by pkl

--- dumpDBmotifDist.py
+++ dumpDBmotifDist.py
@@ -6,7 +6,7 @@ import time
 import getopt
 import cPickle
 from zipfile import ZipFile, ZIP_DEFLATED
-from Stats import loadDB2RAM,getREF_MotifSeq,getDist,getMerged
+from Stats import loadDB2RAM,getREF_MotifSeq,getDist,getMerged,mergeREF_SeqName
 from CAREdb import CAREdb
 
 def getCoDist(distA,distB,lower,higer):
@@ -28,8 +28,8 @@ def getDBmotif_dist(dbname):
     (REF_SeqName,Motifs)=loadDB2RAM(care,None,None,False)
     id2MotifSeq=dict([(v,k) for k,v in care.MotifSeq.items()])
     motif_dist={}
-    for motif in Motifs:
-        print motif
+    for i,motif in enumerate(Motifs):
+        print motif,i
         REF_MotifSeq=getREF_MotifSeq(care,motif)
         motif_dist[motif]={}
         for MotifSeq_id in REF_MotifSeq:
@@ -38,15 +38,56 @@ def getDBmotif_dist(dbname):
         print motif_dist[motif].keys()
     return getMerged(motif_dist,"REF_SeqName")
 
+def getDBmotif_dist_zpkl(dbname):
+    care=CAREdb(dbname)
+    zpklfile="%s_motif_dist_db.zpkl" % dbname.split('.')[0]
+    if os.path.exists(zpklfile):
+        zf = ZipFile(zpklfile, 'r', ZIP_DEFLATED)
+        motifs_zipped=[m.split('.')[0] for m in zf.namelist()]
+        zf.close()
+        print motifs_zipped
+    else:
+        motifs_zipped=[]
+    zf = ZipFile(zpklfile, 'a', ZIP_DEFLATED)
+    print "Loading motif dist in DB..."
+    (REF_SeqName,Motifs)=loadDB2RAM(care,None,None,False)
+    id2MotifSeq=dict([(v,k) for k,v in care.MotifSeq.items()])
+    for i,motif in enumerate(Motifs):
+        print motif,i
+        if motif in motifs_zipped:
+            continue
+        REF_MotifSeq=getREF_MotifSeq(care,motif)
+        motif_dist={}
+        for MotifSeq_id in REF_MotifSeq:
+            MotifSeq=id2MotifSeq[MotifSeq_id]
+            motif_dist[MotifSeq]=getDist(care,MotifSeq_id)
+        motif_dist=mergeREF_SeqName(motif_dist)
+        print motif_dist.keys()
+        zf.writestr('%s.pkl' % motif, cPickle.dumps(motif_dist))
+    zf.close()
+    motif_dist=loadZpkl_motif_dist(zpklfile,Motifs)
+    return motif_dist #getMerged(motif_dist,"REF_SeqName")
+
+def loadZpkl_motif_dist(zpklfile,Motifs):
+    zf = ZipFile(zpklfile, 'r')
+    motif_dist={}
+    for i,motif in enumerate(Motifs):
+        print motif,i
+        motif_dist[motif] = cPickle.loads(zf.open('%s.pkl' % motif).read())
+    return motif_dist
+
 def getCoOccurMotifDB(motif_dist_db,co_dist_range):
     Motifs=motif_dist_db.keys()
     co_Occur_db={}
     for motifa in Motifs:
+        print len(Motifs)
         for motifb in Motifs[1:]:
-            print motifa,motifb
+            #print motifa,motifb
             distances=getCoMotifDist(motifa,motifb,motif_dist_db,co_dist_range)
             if distances:
-                co_Occur_db[(motifa,motifb)]=distances
+                #co_Occur_db[(motifa,motifb)]=distances
+                #为了减小内存占用，仅保存数量信息
+                co_Occur_db[(motifa,motifb)]=len(distances.keys())
         Motifs=Motifs[1:]
     return co_Occur_db
 
@@ -57,6 +98,37 @@ def getCoMotifDist(motifa,motifb,motif_dist_db,co_dist_range):
     for REF_SeqName in REF_SeqNames:
         distA_db=motif_dist_db[motifa][REF_SeqName]
         distB_db=motif_dist_db[motifb][REF_SeqName]
+        distances_db=getCoDist(distA_db,distB_db,lower,higer)
+        if distances_db:
+            motif_pair_dist[REF_SeqName]=distances_db
+    return motif_pair_dist
+
+def getCoOccurMotifDBZpkl(zpklfile,co_dist_range):
+    zf = ZipFile(zpklfile, 'r', ZIP_DEFLATED)
+    Motifs=[m.split('.')[0] for m in zf.namelist()]
+    co_Occur_db={}
+    for motifa in Motifs:
+        print len(Motifs)
+        motif_dist_db_motifa=cPickle.loads(zf.open('%s.pkl' % motifa).read())
+        for motifb in Motifs[1:]:
+            #print motifa,motifb
+            motif_dist_db_motifb=cPickle.loads(zf.open('%s.pkl' % motifb).read())
+            distances=getCoMotifDist(motif_dist_db_motifa,motif_dist_db_motifb,co_dist_range)
+            if distances:
+                #co_Occur_db[(motifa,motifb)]=distances
+                #为了减小内存占用，仅保存数量信息
+                co_Occur_db[(motifa,motifb)]=len(distances.keys())
+        Motifs=Motifs[1:]
+    zf.close()
+    return co_Occur_db
+
+def getCoMotifDistZpkl(motif_dist_db_motifa,motif_dist_db_motifb,co_dist_range):
+    (lower,higer)=co_dist_range
+    REF_SeqNames=set(motif_dist_db_motifa.keys())&set(motif_dist_db_motifb.keys())
+    motif_pair_dist={}
+    for REF_SeqName in REF_SeqNames:
+        distA_db=motif_dist_db_motifa[REF_SeqName]
+        distB_db=motif_dist_db_motifb[REF_SeqName]
         distances_db=getCoDist(distA_db,distB_db,lower,higer)
         if distances_db:
             motif_pair_dist[REF_SeqName]=distances_db
@@ -83,7 +155,7 @@ def has_file(filename):
 if __name__ == '__main__':
     co_dist_range=(10,250)
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "hd:", \
+        opts, args = getopt.getopt(sys.argv[1:], "hd:r:", \
             ["help","database="])
     except getopt.GetoptError:
         usage()
@@ -110,14 +182,15 @@ if __name__ == '__main__':
     if os.path.exists(motif_dist_pkl):
         motif_dist_db=cPickle.loads(open(motif_dist_pkl).read())
     else:
-        motif_dist_db=getDBmotif_dist(dbname)
+        motif_dist_db=getDBmotif_dist_zpkl(dbname)
         savePKL(motif_dist_pkl,motif_dist_db)
 
     co_Occur_pkl='%s_co_Occur_db%s#%s.pkl' % (output,co_dist_range[0],co_dist_range[1])
     co_Occur_db=getCoOccurMotifDB(motif_dist_db,co_dist_range)
     savePKL(co_Occur_pkl,co_Occur_db)
-
+    '''
     co_Occur_slim_pkl='%s_co_Occur_slim%s#%s.pkl' % (output,co_dist_range[0],co_dist_range[1])
     slimCoOccurMotifDB=slimCoOccurMotifDB(co_Occur_db)
     savePKL(co_Occur_slim_pkl,slimCoOccurMotifDB)
+    '''
     print time.time()-timestamp
